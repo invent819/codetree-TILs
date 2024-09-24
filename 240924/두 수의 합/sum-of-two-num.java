568e3eaa7c569b3f4903d35ded5b5fcb3078ff74
invent819 <41150502+invent819@users.noreply.github.com>
2024-09-24 23:56:25 +0900

    Title: 두 수의 합, Time: 450ms, Memory: 25MB, Status: Passed - CodeTree

--- 240924/두 수의 합/sum-of-two-num.java
+++ 240924/두 수의 합/sum-of-two-num.java
@@ -1,39 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int n, k;
-	static int[] nums;
-	static boolean[] visited;
+	static long[] nums;
+	static HashMap<Long, Integer> count = new HashMap<>();
 
 	static int result = 0;
-
-	static void comb(int idx, int cnt, int[] choice) {
-		if (cnt == 2) {
-			if (choice[0] + choice[1] == k) {
-				result++;
-//				System.out.println(Arrays.toString(choice));
-			}
-
-			return;
-		}
-		if (idx == n) {
-			return;
-		}
-
-		for (int i = idx; i < n; i++) {
-			if (!visited[i]) {
-				choice[cnt] = nums[i];
-				visited[i] = true;
-				comb(i + 1, cnt + 1, choice);
-				visited[i] = false;
-
-			}
-		}
-
-	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -43,15 +19,28 @@ public class Main {
 		k = Integer.parseInt(st.nextToken());
 
 		st = new StringTokenizer(br.readLine());
-		nums = new int[n];
-		visited = new boolean[n];
+		nums = new long[n];
+
 		for (int i = 0; i < n; i++) {
 			nums[i] = Integer.parseInt(st.nextToken());
 		}
 
-		comb(0, 0, new int[2]);
+		for (int i = 0; i < n; i++) {
+			long diff = k - nums[i];
 
+			if (count.containsKey(diff)) {
+				result += count.get(diff);
+			}
+
+			if (!count.containsKey(nums[i])) {
+				count.put(nums[i], 1);
+			} else {
+				count.put(nums[i], count.get(nums[i]) + 1);
+			}
+
+		}
 		System.out.println(result);
+
 	}
 
 }
