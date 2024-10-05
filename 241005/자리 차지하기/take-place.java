d3e924af85cb00d6d7f2e47032309d2c21721eb1
invent819 <41150502+invent819@users.noreply.github.com>
2024-10-05 15:03:23 +0900

    Title: 자리 차지하기, Time: 526ms, Memory: 29MB, Status: Passed - CodeTree

--- 241005/자리 차지하기/take-place.java
+++ 241005/자리 차지하기/take-place.java
@@ -6,7 +6,8 @@ import java.util.TreeSet;
 public class Main {
 	static int n, m;
 	static int[][] map;
-	static TreeSet<Integer> treeSet = new TreeSet<>();
+	static int[] arr;
+	static TreeSet<Integer> seats = new TreeSet<>();
 	static boolean[] visited;
 	static int ans = 0;
 
@@ -17,16 +18,22 @@ public class Main {
 		m = Integer.parseInt(st.nextToken());
 		visited = new boolean[m];
 		st = new StringTokenizer(br.readLine());
+		arr = new int[n];
 		for (int i = 0; i < n; i++) {
-			treeSet.add(Integer.parseInt(st.nextToken()) * -1);
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		for (Integer s : treeSet) {
-			int find = (s * -1) - 1;
-			if (!visited[find]) {
-				visited[find] = true;
+		for (int i = 1; i <= m; i++) {
+			seats.add(i);
+		}
+		for (int i = 0; i < n; i++) {
+			if (seats.floor(arr[i]) != null) {
+				int num = seats.floor(arr[i]);
+				seats.remove(num);
 				ans++;
-			}
+			} else
+				break;
 		}
+
 		System.out.println(ans);
 	}
 }
