7b5d755922b821bf52153f3ae6766b5feb2d1105
invent819 <41150502+invent819@users.noreply.github.com>
2024-11-02 02:10:30 +0900

    Title: 이상한 폭탄, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241102/이상한 폭탄/strange-bomb.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int MAX_N = 1_000_000;
+	static int n, k;
+	static Point[] arr = new Point[MAX_N];
+	static int[] L = new int[MAX_N];
+	static int[] R = new int[MAX_N];
+
+	static boolean valid(int creterian, int target) {
+		return creterian - k <= target && target <= creterian + k;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+
+		arr = new Point[n];
+		for (int i = 0; i < n; i++) {
+			int v = Integer.parseInt(br.readLine());
+			arr[i] = new Point(i, v);
+		}
+//		System.out.println(Arrays.toString(arr));
+		Arrays.sort(arr);
+
+		for (int i = 0; i < n - 1; i++) {
+			Point p1 = arr[i];
+			Point p2 = arr[i+1];
+			if(p1.v == p2.v && valid(p1.x, p2.x)) {
+				System.out.println(p1.v);
+				break;
+			}
+		}
+
+	}
+
+}
+
+class Point implements Comparable<Point> {
+	int x, v;
+
+	Point(int x, int v) {
+		this.x = x;
+		this.v = v;
+	}
+
+	@Override
+	public int compareTo(Point o) {
+		if(o.v != v) {
+			return Integer.compare(o.v, v);
+		}
+		return Integer.compare(o.x, x);
+	}
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", v=" + v + "]";
+	}
+}
