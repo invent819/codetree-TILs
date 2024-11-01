615136fa3624c63feeca6564658b9fe2c7c119f9
invent819 <41150502+invent819@users.noreply.github.com>
2024-11-02 00:46:32 +0900

    Title: 구간 외 최대 숫자, Time: 97ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 241102/구간 외 최대 숫자/max-num-outside-of-interval.java
@@ -0,0 +1,42 @@
+package No;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int MAX_N = 100_000;
+	static int n, q;
+	static int[] arr = new int[MAX_N];
+	static int[] L = new int[MAX_N];
+	static int[] R = new int[MAX_N];
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		q = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int maxLN = Integer.MIN_VALUE;
+		int maxRN = Integer.MIN_VALUE;
+		for (int i = 0; i < n; i++) {
+			L[i] = Math.max(maxLN, arr[i]);
+			R[n - i - 1] = Math.max(maxRN, arr[n - 1 - i]);
+		}
+
+		for (int i = 0; i < q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			System.out.println(Math.max(L[a - 2], R[b ]));
+		}
+	}
+
+}
