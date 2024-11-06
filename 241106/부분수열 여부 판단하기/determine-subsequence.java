ab1f004710e57598facfdd618beda57f61f6d5b9
invent819 <41150502+invent819@users.noreply.github.com>
2024-11-06 16:47:18 +0900

    Title: 부분수열 여부 판단하기, Time: 3033ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

---
+++ 241106/부분수열 여부 판단하기/determine-subsequence.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m, s;
+
+	public static int MAX_N = 100000;
+	public static int[] arr = new int[MAX_N];
+	public static int[] L = new int[MAX_N];
+	public static int[] R = new int[MAX_N];
+	public static int ans = 0;
+	static int r, c, l;
+	static int[][] graph;
+
+	static StringBuilder sb = new StringBuilder();
+	static boolean[][] visited;
+
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
+	static int[] nums = new int[MAX_N + 1];
+
+	static long[] dp;
+	static int[] a, b;
+
+	static boolean subNums(int aIdx, int bIdx) {
+		if (aIdx == n) {
+			return false;
+		}
+
+		for (int i = aIdx; i < n; i++) {
+			if (a[i] == b[bIdx]) {
+				if (bIdx == m - 1) {
+					return true;
+				}
+
+				boolean ret = subNums(i + 1, bIdx + 1);
+				if (ret) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		a = new int[n];
+		b = new int[m];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < m; i++) {
+			b[i] = Integer.parseInt(st.nextToken());
+		}
+
+		System.out.println(subNums(0, 0) ? "Yes" : "No");
+
+//		System.out.println(ans);
+	}
+}
